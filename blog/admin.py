4f9b7febfc8ff9c5d75f9be3284d501c70920e86
Ross24 <56484729+Ross24@users.noreply.github.com>
2020-04-29 00:34:25 +0000

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+#register our models so they show up on our admin page
+from .models import Post, Comment  
+admin.site.register(Post)
+
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
+admin.site.register(Comment, CommentAdmin)
